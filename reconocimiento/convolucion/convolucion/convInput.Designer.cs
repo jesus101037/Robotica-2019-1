f1799491904cbf5152f2f7c2f5e92e6cab7e75e1
JFUF <101037@unsaac.edu.pe>
2019-09-08 13:22:18 -0500

    no message

---
+++ reconocimiento/convolucion/convolucion/convInput.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace convolucion
+{
+    partial class convInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buAplicarFiltro = new System.Windows.Forms.Button();
+            this.pbFiltro = new System.Windows.Forms.PictureBox();
+            this.dgvFiltro = new System.Windows.Forms.DataGridView();
+            this.pbOrigen = new System.Windows.Forms.PictureBox();
+            this.buCargarImg = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbFiltroN = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFiltro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiltro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOrigen)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buAplicarFiltro
+            // 
+            this.buAplicarFiltro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buAplicarFiltro.Location = new System.Drawing.Point(483, 3);
+            this.buAplicarFiltro.Name = "buAplicarFiltro";
+            this.buAplicarFiltro.Size = new System.Drawing.Size(314, 37);
+            this.buAplicarFiltro.TabIndex = 12;
+            this.buAplicarFiltro.Text = "Aplicar filtro";
+            this.buAplicarFiltro.UseVisualStyleBackColor = true;
+            this.buAplicarFiltro.Click += new System.EventHandler(this.aplicarFiltro_click);
+            // 
+            // pbFiltro
+            // 
+            this.pbFiltro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFiltro.Location = new System.Drawing.Point(483, 46);
+            this.pbFiltro.Name = "pbFiltro";
+            this.pbFiltro.Size = new System.Drawing.Size(314, 401);
+            this.pbFiltro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFiltro.TabIndex = 11;
+            this.pbFiltro.TabStop = false;
+            // 
+            // dgvFiltro
+            // 
+            this.dgvFiltro.AllowUserToAddRows = false;
+            this.dgvFiltro.AllowUserToResizeColumns = false;
+            this.dgvFiltro.AllowUserToResizeRows = false;
+            this.dgvFiltro.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvFiltro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiltro.ColumnHeadersVisible = false;
+            this.dgvFiltro.Location = new System.Drawing.Point(3, 157);
+            this.dgvFiltro.Name = "dgvFiltro";
+            this.dgvFiltro.RowHeadersVisible = false;
+            this.dgvFiltro.Size = new System.Drawing.Size(148, 131);
+            this.dgvFiltro.TabIndex = 10;
+            // 
+            // pbOrigen
+            // 
+            this.pbOrigen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbOrigen.Location = new System.Drawing.Point(3, 46);
+            this.pbOrigen.Name = "pbOrigen";
+            this.pbOrigen.Size = new System.Drawing.Size(314, 401);
+            this.pbOrigen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOrigen.TabIndex = 8;
+            this.pbOrigen.TabStop = false;
+            // 
+            // buCargarImg
+            // 
+            this.buCargarImg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buCargarImg.Location = new System.Drawing.Point(3, 3);
+            this.buCargarImg.Name = "buCargarImg";
+            this.buCargarImg.Size = new System.Drawing.Size(314, 37);
+            this.buCargarImg.TabIndex = 7;
+            this.buCargarImg.Text = "Cargar Imagen";
+            this.buCargarImg.UseVisualStyleBackColor = true;
+            this.buCargarImg.Click += new System.EventHandler(this.buscarImg_click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buCargarImg, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buAplicarFiltro, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbOrigen, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pbFiltro, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.777778F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.22222F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(323, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 43);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seleccionar Filtro";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buCancelar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbFiltroN);
+            this.panel1.Controls.Add(this.dgvFiltro);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(323, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(154, 401);
+            this.panel1.TabIndex = 15;
+            // 
+            // cbFiltroN
+            // 
+            this.cbFiltroN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFiltroN.FormattingEnabled = true;
+            this.cbFiltroN.Items.AddRange(new object[] {
+            "3",
+            "5"});
+            this.cbFiltroN.Location = new System.Drawing.Point(3, 100);
+            this.cbFiltroN.Name = "cbFiltroN";
+            this.cbFiltroN.Size = new System.Drawing.Size(148, 21);
+            this.cbFiltroN.TabIndex = 11;
+            this.cbFiltroN.SelectedIndexChanged += new System.EventHandler(this.cbFiltroN_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Tamanio Filtro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Filtro:";
+            // 
+            // buCancelar
+            // 
+            this.buCancelar.Location = new System.Drawing.Point(40, 294);
+            this.buCancelar.Name = "buCancelar";
+            this.buCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buCancelar.TabIndex = 14;
+            this.buCancelar.Text = "Cancelar";
+            this.buCancelar.UseVisualStyleBackColor = true;
+            this.buCancelar.Click += new System.EventHandler(this.buCancelar_Click);
+            // 
+            // convInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "convInput";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFiltro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiltro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOrigen)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buAplicarFiltro;
+        private System.Windows.Forms.PictureBox pbFiltro;
+        private System.Windows.Forms.DataGridView dgvFiltro;
+        private System.Windows.Forms.PictureBox pbOrigen;
+        private System.Windows.Forms.Button buCargarImg;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buCancelar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbFiltroN;
+    }
+}
+
---
+++ reconocimiento/convolucion/convolucion/convInput.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convolucion
+{
+    public partial class convInput : Form
+    {
+        /// <summary>
+        /// Tamanio de la matriz de filtro
+        /// </summary>
+        int filtroN = 3;
+
+        public convInput()
+        {
+            InitializeComponent();
+            inicializar();
+        }
+
+        public void inicializar() {
+            filtroN = 3;
+            cbFiltroN.SelectedIndex = 0;
+            pbOrigen.Image = pbOrigen.ErrorImage;
+            pbFiltro.Image = pbFiltro.ErrorImage;
+            inicializarFiltro();
+        }
+        public void inicializarFiltro() {
+            for (int i = 0; i < filtroN; i++)
+            {
+                for (int j = 0; j < filtroN; j++)
+                {
+                    dgvFiltro[i, j].Value = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Evalua que <paramref name="Valor"/> esta en el rango [0;255]
+        /// </summary>
+        /// <param name="Valor">Valor a evaluar</param>
+        /// <returns></returns>
+        public int analisisValor(int Valor) {
+            if (Valor < 0)
+            {
+                Valor = 0;
+            }
+            else if (Valor > 255)
+            {
+                Valor = 255;
+            }
+            return Valor;
+        }
+
+        /// <summary>
+        /// Aplica el operador de convolucion para un pixel en la posción central de <paramref name="subMatriz"/>
+        /// con un filtro definido en <paramref name="filtro"/>.
+        /// </summary>
+        /// <param name="subMatriz">Sub matriz de la imagen del mismo tamanio que <paramref name="filtro"/></param>
+        /// <param name="filtro"></param>
+        /// <returns>Resultado de aplicar la convolucion entre <paramref name="subMatriz"/> y <paramref name="filtro"/></returns>
+        public int convolucionPixel(int[,] subMatriz, double[,] filtro) {
+            int resultado = 0;
+            double parcial = 0;
+            int longitud = subMatriz.GetLength(0);
+            for (int i = 0; i < longitud; i++)
+            {
+                for (int j = 0; j < longitud; j++)
+                {
+                    parcial = parcial + subMatriz[i, j] * filtro[i, j];
+                }
+            }
+            resultado = Convert.ToInt32(parcial);
+            return analisisValor(resultado);
+        }
+        /// <summary>
+        /// Obtienen una matriz de <paramref name="n"/>x<paramref name="n"/> que rodea al elemento
+        /// pivot en la posicion (<paramref name="x"/>, <paramref name="y"/>) de <paramref name="matriz"/>
+        /// </summary>
+        /// <param name="matriz">Matriz origen</param>
+        /// <param name="x">Coordenada en x del pivot</param>
+        /// <param name="y">Coordenada en y del pivot</param>
+        /// <param name="n">Tamanio de la matriz cuadrada, debe ser impar</param>
+        /// <returns></returns>
+        public int[,] obtenerSubmatriz(int[,] matriz, int x, int y, int n) {
+            int[,] subMatriz = new int[n, n];
+            int r = (n / 2);
+            int maxX = matriz.GetLength(0);
+            int maxY = matriz.GetLength(1);
+            int xMin = 0;
+            int yMin = 0;
+            int xMax = 0;
+            int yMax = 0;
+            for (int i = 0; i < n; i++)
+            {
+                xMin = x + i - r;
+                xMax = x + i + r;
+                if (xMin >= 0 && xMax < maxX)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        yMin = y + j - r;
+                        yMax = y + j + r;
+                        if (yMin >= 0 && yMax < maxY)
+                        {
+                            subMatriz[i, j] = matriz[xMin, yMin];
+                        }
+                    }
+                }
+            }
+            return subMatriz;
+        }
+
+        /// <summary>
+        /// Aplica la operacion de convolución a cada pixel de cada canal RGB de la imagen
+        /// <paramref name="img"/> con el filtro <paramref name="filtro"/>
+        /// </summary>
+        /// <param name="img">Imagen a la cual se desea aplicar el filtro</param>
+        /// <param name="filtro">Filtro a aplicar a la imagen, debe ser una matriz impar</param>
+        /// <returns></returns>
+        public Bitmap aplicarFiltro(Bitmap img, double[,] filtro) {
+            int n = filtro.GetLength(0);
+            Bitmap imgR = new Bitmap(img.Width, img.Height);
+            // Variables que contienen la posicion del pivot
+            //int x, y;
+            // Matrices q representa la imagen original en sistema RGB
+            int[,] imaR = new int[img.Width, img.Height];
+            int[,] imaG = new int[img.Width, img.Height];
+            int[,] imaB = new int[img.Width, img.Height];
+            // Matrices que contendra la imagen en sistema RGB despues de aplicar el filtro
+            int[,] imaRR = new int[img.Width, img.Height];
+            int[,] imaRG = new int[img.Width, img.Height];
+            int[,] imaRB = new int[img.Width, img.Height];
+            // Generamos las matrices con los valores de los canales RGB de la imagen.
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    Color oc = img.GetPixel(x, y);
+                    imaR[x, y] = (int)(oc.R);
+                    imaG[x, y] = (int)(oc.G);
+                    imaB[x, y] = (int)(oc.B);
+                }
+            }
+
+            for (int x = 0; x < img.Width - 1; x++)
+            {
+                
+                for (int y = 0; y < img.Height - 1; y++)
+                {
+
+                    imaRR[x, y] = convolucionPixel(obtenerSubmatriz(imaR, x, y, n), filtro);
+                    imaRG[x, y] = convolucionPixel(obtenerSubmatriz(imaG, x, y, n), filtro);
+                    imaRB[x, y] = convolucionPixel(obtenerSubmatriz(imaB, x, y, n), filtro);
+
+                    Color oc = img.GetPixel(x, y);
+
+                    int colorR = analisisValor(imaRR[x, y]);
+                    int colorG = analisisValor(imaRG[x, y]);
+                    int colorB = analisisValor(imaRB[x, y]);
+                    Color nc = Color.FromArgb(oc.A, colorR, colorG, colorB);
+
+                    imgR.SetPixel(x, y, nc);
+                }
+            }
+            return imgR;
+        }
+
+        public double[,] obtenerFiltro() {
+            double[,] filtro = new double[filtroN, filtroN];
+            for (int i = 0; i < filtroN; i++)
+            {
+                for (int j = 0; j < filtroN; j++)
+                {
+                    string aux = dgvFiltro[j, i].Value.ToString();
+                    filtro[i, j] = double.Parse(aux);
+                }
+            }
+            return filtro;
+        }
+
+        private void buscarImg_click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;...";
+            dialog.InitialDirectory = @"C:\";
+            dialog.Title = "Seleccione image.";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Recuperar imagen.
+                Bitmap image = new Bitmap(new Bitmap(dialog.FileName),pbOrigen.Width, pbOrigen.Height);
+                pbOrigen.Image = image;
+            }
+        }
+
+        private void aplicarFiltro_click(object sender, EventArgs e)
+        {
+            //pictureBox2 = new PictureBox();
+            Bitmap im = (Bitmap)(pbOrigen.Image);
+            pbFiltro.Image = aplicarFiltro(im, obtenerFiltro());
+        }
+
+        private void buCancelar_Click(object sender, EventArgs e)
+        {
+            inicializar();
+        }
+
+        private void cbFiltroN_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filtroN = int.Parse(cbFiltroN.Items[cbFiltroN.SelectedIndex].ToString());
+            dgvFiltro.ColumnCount = filtroN;
+            dgvFiltro.RowCount = filtroN;
+            inicializarFiltro();
+        }
+    }
+}
