702af7f1dd55ce186ef8086aa3724eca8234acc6
JFUF <101037@unsaac.edu.pe>
2019-09-08 14:44:16 -0500

    no message

---
+++ reconocimiento/lineasCirculos/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbInput = new System.Windows.Forms.PictureBox();
+            this.pbProcesado = new System.Windows.Forms.PictureBox();
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nudIP1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudIP2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudIP3 = new System.Windows.Forms.NumericUpDown();
+            this.nudIP4 = new System.Windows.Forms.NumericUpDown();
+            this.buConectar = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.buDesconectar = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudPort = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProcesado)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pbProcesado, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbInput, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rtbConsole, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(403, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 219);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbInput
+            // 
+            this.pbInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbInput.Location = new System.Drawing.Point(3, 3);
+            this.pbInput.Name = "pbInput";
+            this.pbInput.Size = new System.Drawing.Size(394, 219);
+            this.pbInput.TabIndex = 1;
+            this.pbInput.TabStop = false;
+            // 
+            // pbProcesado
+            // 
+            this.pbProcesado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbProcesado.Location = new System.Drawing.Point(3, 228);
+            this.pbProcesado.Name = "pbProcesado";
+            this.pbProcesado.Size = new System.Drawing.Size(394, 219);
+            this.pbProcesado.TabIndex = 2;
+            this.pbProcesado.TabStop = false;
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbConsole.Location = new System.Drawing.Point(403, 228);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.ReadOnly = true;
+            this.rtbConsole.Size = new System.Drawing.Size(394, 219);
+            this.rtbConsole.TabIndex = 3;
+            this.rtbConsole.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buDesconectar);
+            this.groupBox1.Controls.Add(this.nudPort);
+            this.groupBox1.Controls.Add(this.buConectar);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.nudIP4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nudIP3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nudIP2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.nudIP1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 79);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camara IP";
+            // 
+            // nudIP1
+            // 
+            this.nudIP1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.nudIP1.Location = new System.Drawing.Point(36, 19);
+            this.nudIP1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudIP1.Name = "nudIP1";
+            this.nudIP1.Size = new System.Drawing.Size(45, 20);
+            this.nudIP1.TabIndex = 0;
+            this.nudIP1.Value = new decimal(new int[] {
+            192,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "-";
+            // 
+            // nudIP2
+            // 
+            this.nudIP2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.nudIP2.Location = new System.Drawing.Point(96, 19);
+            this.nudIP2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudIP2.Name = "nudIP2";
+            this.nudIP2.Size = new System.Drawing.Size(45, 20);
+            this.nudIP2.TabIndex = 3;
+            this.nudIP2.Value = new decimal(new int[] {
+            168,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(203, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // nudIP3
+            // 
+            this.nudIP3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.nudIP3.Location = new System.Drawing.Point(157, 19);
+            this.nudIP3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudIP3.Name = "nudIP3";
+            this.nudIP3.Size = new System.Drawing.Size(45, 20);
+            this.nudIP3.TabIndex = 5;
+            this.nudIP3.Value = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            // 
+            // nudIP4
+            // 
+            this.nudIP4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.nudIP4.Location = new System.Drawing.Point(219, 19);
+            this.nudIP4.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudIP4.Name = "nudIP4";
+            this.nudIP4.Size = new System.Drawing.Size(45, 20);
+            this.nudIP4.TabIndex = 7;
+            this.nudIP4.Value = new decimal(new int[] {
+            89,
+            0,
+            0,
+            0});
+            // 
+            // buConectar
+            // 
+            this.buConectar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buConectar.Location = new System.Drawing.Point(106, 45);
+            this.buConectar.Name = "buConectar";
+            this.buConectar.Size = new System.Drawing.Size(85, 23);
+            this.buConectar.TabIndex = 1;
+            this.buConectar.Text = "Conectar";
+            this.buConectar.UseVisualStyleBackColor = true;
+            this.buConectar.Click += new System.EventHandler(this.buIniciar_Click);
+            // 
+            // buDesconectar
+            // 
+            this.buDesconectar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buDesconectar.Location = new System.Drawing.Point(197, 45);
+            this.buDesconectar.Name = "buDesconectar";
+            this.buDesconectar.Size = new System.Drawing.Size(85, 23);
+            this.buDesconectar.TabIndex = 2;
+            this.buDesconectar.Text = "Desconectar";
+            this.buDesconectar.UseVisualStyleBackColor = true;
+            this.buDesconectar.Click += new System.EventHandler(this.buTerminar_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = ":";
+            // 
+            // nudPort
+            // 
+            this.nudPort.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.nudPort.Location = new System.Drawing.Point(289, 19);
+            this.nudPort.Maximum = new decimal(new int[] {
+            64738,
+            0,
+            0,
+            0});
+            this.nudPort.Name = "nudPort";
+            this.nudPort.Size = new System.Drawing.Size(63, 20);
+            this.nudPort.TabIndex = 9;
+            this.nudPort.Value = new decimal(new int[] {
+            4747,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProcesado)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIP4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pbProcesado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buConectar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nudIP4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudIP3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudIP2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudIP1;
+        private System.Windows.Forms.PictureBox pbInput;
+        private System.Windows.Forms.RichTextBox rtbConsole;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button buDesconectar;
+        private System.Windows.Forms.NumericUpDown nudPort;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ reconocimiento/lineasCirculos/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using System.Drawing.Imaging;
+using AForge.Imaging.Filters;
+using AForge;
+using AForge.Imaging;
+using AForge.Math.Geometry;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MJPEGStream stream;
+        Bitmap bmp;
+
+        bool Ingreso = false;
+        bool Hay2Circulos = false;
+
+        int tiempoGiro;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string url = "http://" + obtenerIP() + "/mjpegfeed?640x480";
+            stream = new MJPEGStream(url);
+            stream.NewFrame += streamNewFrame;
+            tiempoGiro = 2000;
+        }
+
+        private string obtenerIP() {
+            string ip = $"{nudIP1.Value.ToString()}.{nudIP2.Value.ToString()}.{nudIP3.Value.ToString()}.{nudIP4.Value.ToString()}:{nudPort.Value.ToString()}";
+            return ip;
+        }
+
+        private void streamNewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            bmp = (Bitmap)eventArgs.Frame.Clone();
+            pbInput.Image = bmp;
+
+            if (!Ingreso)
+            {
+                backgroundWorker1.RunWorkerAsync(bmp);
+                Ingreso = true;
+            }
+        }
+
+        delegate void pasarImagen(Bitmap Mapa);
+
+        private void mostrarImagen(Bitmap Mapa)
+        {
+            pbProcesado.Image = Mapa;
+        }
+
+        delegate void Pasar_Datos(List<string> Datos);
+
+        private void Mostrar_Datos(List<string> Datos)
+        {
+            if (Datos.Count == 2)
+            {
+                string[] Datos1 = Datos[0].Split('-');
+                lbl_C1X.Text = Datos1[0];
+                lbl_C1Y.Text = Datos1[1];
+                lbl_Radio1.Text = Datos1[2];
+
+                string[] Datos2 = Datos[1].Split('-');
+                lbl_C2X.Text = Datos2[0];
+                lbl_C2Y.Text = Datos2[1];
+                lbl_Radio2.Text = Datos2[2];
+                Hay2Circulos = true;
+            }
+            else
+            {
+                Hay2Circulos = false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap bmp2 = (Bitmap)e.Argument;
+
+            //bmp2 = Escala_Grises_Borde((Bitmap)ImgReducida.Clone());
+            bmp2 = MostrarAzul(bmp2);
+
+            List<string> Datos = Procesar_Circulos(bmp2);
+
+            Pasar_Datos pd = new Pasar_Datos(Mostrar_Datos);
+            this.Invoke(pd, Datos);
+
+            pasarImagen pa = new pasarImagen(mostrarImagen);
+            this.Invoke(pa, bmp2);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (Hay2Circulos)
+            {
+                //Hacer que el carro gire avance y vuelva a girar
+                if (float.Parse(lbl_Radio1.Text) - float.Parse(lbl_Radio2.Text) > 2)
+                {
+                    double radio1 = double.Parse(lbl_Radio1.Text);
+                    double Distancia1 = 4.5 * 0.22 / radio1 / 100;
+
+                    //lbl_DistanciaCirculo1.Text = Distancia1.ToString();
+
+                    double radio2 = double.Parse(lbl_Radio2.Text);
+                    double Distancia2 = 4.5 * 0.22 / radio2 / 100;
+                    //lbl_DistanciaCirculo2.Text = Distancia2.ToString();
+
+
+                    double Tiempo = Math.Abs(Distancia1 - Distancia2) / 5;
+
+                    Pasar_Distancias pa = new Pasar_Distancias(Mostrar_Distancia);
+                    string a = Distancia1.ToString() + "-" + Distancia2.ToString();
+
+                    this.Invoke(pa, a);
+
+                    //giro a la derecha
+                    serialPort1.Write("d");
+                    System.Threading.Thread.Sleep(tiempoGiro);
+
+                    //avanzar
+                    serialPort1.Write("w");
+                    System.Threading.Thread.Sleep(Convert.ToInt32(Tiempo * 1000));
+
+                    //giro a la izquierda
+                    tiempoGiro += 500;
+                    serialPort1.Write("a");
+                    System.Threading.Thread.Sleep(tiempoGiro);
+
+                }
+                else
+                {
+                    serialPort1.Write("w");
+                }
+            }
+
+            backgroundWorker1.Dispose();
+            backgroundWorker1.CancelAsync();
+            backgroundWorker1.RunWorkerAsync(bmp);
+        }
+
+        //video
+        private void buIniciar_Click(object sender, EventArgs e)
+        {
+            stream.Start();
+        }
+
+        private void buTerminar_Click(object sender, EventArgs e)
+        {
+            stream.Stop();
+        }
+
+        public Bitmap MostrarAzul(Bitmap Mapa)
+        {
+            Bitmap AuxMapa = Mapa;
+            for (int x = 1; x < AuxMapa.Width - 1; x++)
+            {
+                for (int y = 1; y < AuxMapa.Height - 1; y++)
+                {
+                    Color oc = AuxMapa.GetPixel(x, y);
+                    Color nc = Color.FromArgb(oc.A, 0, 0, oc.B);
+
+                    if ((oc.B > 100) && (oc.G < 100) && (oc.R < 100))
+                    {
+                        nc = Color.FromArgb(oc.A, 0, 0, 255);
+                    }
+                    else if ((oc.R > 100) && (oc.G < 100) && (oc.B < 100))
+                    {
+                        nc = Color.FromArgb(oc.A, 255, 0, 0);
+                    }
+                    else
+                    {
+                        nc = Color.FromArgb(oc.A, 0, 0, 0);
+                    }
+
+                    AuxMapa.SetPixel(x, y, nc);
+                }
+            }
+            return AuxMapa;
+        }
+
+        public List<string> Procesar_Circulos(Bitmap BMP)
+        {
+
+            List<string> Datos = new List<string>();
+
+            Rectangle RECTANGULO = new Rectangle(0, 0, BMP.Width, BMP.Height);
+            BitmapData BMPDATOS = BMP.LockBits(RECTANGULO, ImageLockMode.ReadWrite, BMP.PixelFormat);
+
+            Bitmap A = new Bitmap(BMP.Width, BMP.Height);
+
+            ColorFiltering FILTRO = new ColorFiltering();
+
+            FILTRO.Red = new IntRange(0, 100);
+            FILTRO.Green = new IntRange(0, 100);
+            FILTRO.Blue = new IntRange(0, 100);
+
+            FILTRO.FillOutsideRange = false;
+            FILTRO.ApplyInPlace(BMPDATOS);
+
+            //BUSCA LOS ELEMENTOS
+            BlobCounter ELEMENTOS = new BlobCounter();
+            ELEMENTOS.FilterBlobs = true;
+            ELEMENTOS.MinHeight = 5; //ALTURA MINIMA
+            ELEMENTOS.MinWidth = 5;
+
+            ELEMENTOS.ProcessImage(BMPDATOS);
+
+            Blob[] ELEMENTOSINFO = ELEMENTOS.GetObjectsInformation();
+            BMP.UnlockBits(BMPDATOS);
+
+            SimpleShapeChecker BUSCADOR = new SimpleShapeChecker(); //PARA DETERMINAR LA FORMA DE LOS ELEMENTOS ENCONTRADOS
+            Graphics DIBUJO = Graphics.FromImage(BMP);//PARA DIBUJAR LOS ELEMENTOS
+            Graphics DIBUJO2 = Graphics.FromImage(A);
+
+            Pen CIRCULOS = new Pen(Color.Black, 5); //CIRCULOS
+            Pen TRIANGULOS = new Pen(Color.Black, 5); //TRIANGULOS
+            Pen CUADRILATEROS = new Pen(Color.Black, 5); //CUADRILATEROS
+            Pen TRAZO = new Pen(Color.Red); //'PARA SEÑALIZAR LAS FORMAS
+
+
+            for (int i = 0; i < ELEMENTOSINFO.Length; i++)
+            {
+
+                System.Collections.Generic.List<AForge.IntPoint> PUNTOS = ELEMENTOS.GetBlobsEdgePoints(ELEMENTOSINFO[i]);//'OBTIENE LOS PUNTOS DE LA FORMA
+                AForge.Point CENTRO = new AForge.Point(); //'CENTRO DEL CIRCULO
+                float RADIO = new float(); //'RADIO DEL CIRCULO
+
+                if (BUSCADOR.IsCircle(PUNTOS, out CENTRO, out RADIO))//
+                {//'SI ES UN CIRCULO....
+                    DIBUJO.DrawEllipse(CIRCULOS, (int)Math.Round(CENTRO.X - RADIO), (int)Math.Round(CENTRO.Y - RADIO), (int)Math.Round(RADIO * 2), (int)Math.Round(RADIO * 2));// 'DIBUJA EL CIRCULO
+                    DIBUJO2.DrawEllipse(CIRCULOS, (int)Math.Round(CENTRO.X - RADIO), (int)Math.Round(CENTRO.Y - RADIO), (int)Math.Round(RADIO * 2), (int)Math.Round(RADIO * 2));// 'DIBUJA EL CIRCULO
+
+                    Datos.Add(CENTRO.X + "-" + CENTRO.Y + "-" + RADIO);
+                }
+            }
+
+            //Mostrar_Imagen A
+            pasarImagen pa = new pasarImagen(mostrarImagen);
+            this.Invoke(pa, A);
+
+            return Datos;
+        }
+    }
+}
